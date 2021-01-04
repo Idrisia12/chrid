b41aa6fa7d6905eac073e98817727fa8d0b8699a
Abdulrasheed Idris <66267919+Idrisia12@users.noreply.github.com>
2021-01-04 08:29:31 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { MdFingerprint } from 'react-icons/md';
